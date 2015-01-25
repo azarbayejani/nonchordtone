261e36d1171bbd3a1fa7e944a0af41e43e26169e
Bobak Azarbayejani <bobby.azarbayejani@gmail.com>
2015-01-25 11:01:54 -0800

    Note Create / Show

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,4 +1,18 @@
 class NotesController < ApplicationController
     def new
     end
+
+    def create
+        @note = Note.new(user_params)
+        @note.save
+        redirect_to @note
+    end
+
+    def show
+        @note = Note.find(params[:id])
+    end
+
+    def user_params
+        params.require(:note).permit(:text)
+    end
 end
