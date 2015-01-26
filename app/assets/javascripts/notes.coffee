063e65dcc1e3fcd2288c950bf2eaeb9969b2e03c
Bobak Azarbayejani <bobby.azarbayejani@gmail.com>
2015-01-25 23:33:55 -0800

    Bootstrapifies everything

--- app/assets/javascripts/notes.coffee
+++ app/assets/javascripts/notes.coffee
@@ -1,3 +1,19 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
+
+ready = ->
+  $('#showText').click ->
+    $.ajax({
+      url: window.location,
+      type: "DELETE",
+      success: (result) ->
+        $("#warning-message").hide().remove();
+        $actualMessage = $("#actual-message");
+        $actualMessage.find("#message").text(result.text);
+        $actualMessage.find("input").val(window.location.origin + window.location.pathname.split("/").slice(0,-1).join("/") + "/" + result.new_uid);
+        $actualMessage.show().removeClass("hidden");
+    });
+
+$(document).ready(ready)
+$(document).on('page:load', ready)
--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,30 +1,38 @@
 class NotesController < ApplicationController
-    def new
-    end
 
-    def create
-        @note = Note.new(user_params)
-        @note.save
-        redirect_to action: 'show', uid: @note.uid
-    end
+  rescue_from ActiveRecord::RecordNotFound, :with => :record_not_found
 
-    def show
-        @note = Note.find_by! uid: params[:uid]
-    end
+  def new
+  end
 
-    def destroy
-        old_note = Note.find_by! uid: params[:uid]
-        new_note = old_note.dup
-        new_note.save
-        old_note.destroy
-        render :json => { 
-            :uid => old_note.uid,
-            :text => old_note.text,
-            :new_uid => new_note.uid
-        }
-    end
+  def create
+    @note = Note.new(user_params)
+    @note.save
+    redirect_to action: 'show', uid: @note.uid
+  end
 
-    def user_params
-        params.require(:note).permit(:text)
-    end
+  def show
+    @note = Note.find_by! uid: params[:uid]
+  end
+
+  def destroy
+    old_note = Note.find_by! uid: params[:uid]
+    new_note = old_note.dup
+    new_note.save
+    old_note.destroy
+    render :json => { 
+      :uid => old_note.uid,
+      :text => old_note.text,
+      :new_uid => new_note.uid
+    }
+  end
+
+  def record_not_found
+    render 'record_not_found'
+  end
+
+  private
+  def user_params
+    params.require(:note).permit(:text)
+  end
 end
